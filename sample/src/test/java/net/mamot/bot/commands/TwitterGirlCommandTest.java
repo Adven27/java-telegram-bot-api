8eaec882a3a090aacb8390e52de8d94e5a9cf03f
sbt-toropanov-na <NAToropanov.SBT@sberbank.ru>
2017-03-07 18:39:18 +0300

    smallfix

--- sample/src/test/java/net/mamot/bot/commands/TwitterGirlCommandTest.java
+++ sample/src/test/java/net/mamot/bot/commands/TwitterGirlCommandTest.java
@@ -7,20 +7,20 @@ import static com.pengrad.telegrambot.tester.BotTester.given;
 import static com.pengrad.telegrambot.tester.BotTester.message;
 import static net.mamot.bot.commands.TwitterGirlCommand.COMMAND;
 import static net.mamot.bot.commands.TwitterGirlCommand.GIRL_NAME_IN_TWITTER;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.*;
 
 public class TwitterGirlCommandTest {
     private final TwitterService twitter = mock(TwitterService.class);
     private TwitterGirlCommand sut = new TwitterGirlCommand(twitter);
 
     @Test
-    public void shouldReturnMessageFromAdviceResource() throws Exception {
+    public void shouldReturnTweetFromTwitter() throws Exception {
         when(twitter.getLatestTweet(GIRL_NAME_IN_TWITTER)).thenReturn("yet another stupid tweet");
 
         given(sut).
             got(COMMAND).
         then().
             shouldAnswer(message("yet another stupid tweet"));
+        verify(twitter).getLatestTweet(GIRL_NAME_IN_TWITTER);
     }
 }
