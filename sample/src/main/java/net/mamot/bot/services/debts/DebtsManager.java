e397854f888bed9e80f1432e756988f00f464de2
sbt-toropanov-na <NAToropanov.SBT@sberbank.ru>
2017-03-16 18:31:19 +0300

    localization refactor

--- sample/src/main/java/net/mamot/bot/services/debts/DebtsManager.java
+++
@@ -1,8 +0,0 @@
-package net.mamot.bot.services.debts;
-
-import com.pengrad.telegrambot.model.User;
-
-public interface DebtsManager {
-    WizardStep stateFor(User user);
-    WizardStep updateFor(User user, String data);
-}
