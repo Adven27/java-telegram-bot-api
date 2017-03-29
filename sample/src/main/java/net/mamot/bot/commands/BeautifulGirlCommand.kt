88db75b3c01febdd74fdfb0cc19d1ccd58987471
Anton Korenev <ikor09@gmail.com>
2017-03-29 20:40:18 +0300

    make-me-happy command production-testing-development

--- sample/src/main/java/net/mamot/bot/commands/BeautifulGirlCommand.kt
+++ sample/src/main/java/net/mamot/bot/commands/BeautifulGirlCommand.kt
@@ -5,6 +5,15 @@ import com.pengrad.telegrambot.commands.MessageCommand
 import com.pengrad.telegrambot.model.Chat
 import com.pengrad.telegrambot.model.User
 import com.pengrad.telegrambot.request.SendMessage.message
+import com.pengrad.telegrambot.request.SendPhoto
+
+import org.apache.commons.io.IOUtils
+import java.net.URL
+import javax.imageio.ImageIO
+import java.io.IOException
+import java.io.InputStream
+import java.io.ByteArrayOutputStream
+
 
 /**
  * Created by k1per on 19.03.2017.
@@ -12,11 +21,34 @@ import com.pengrad.telegrambot.request.SendMessage.message
 class BeautifulGirlCommand : MessageCommand(commandName, "BeautifulGirl") {
 
     override fun execute(bot: TelegramBot?, user: User?, chat: Chat?, params: String?) {
-        bot?.execute(message(chat, randomImageUrl))
+        val imageURL = URL(randomImageUrl)
+        val originalImage = ImageIO.read(imageURL)
+        val baos = ByteArrayOutputStream()
+        ImageIO.write(originalImage, "jpg", baos)
+
+        val request = SendPhoto(chat?.id(), baos.toByteArray()).caption("caption")
+        bot?.execute(request)
     }
+
 
     companion object {
         var commandName = "/makeMeHappy"
         var randomImageUrl = "http://loremflickr.com/1280/1024/girl, sexy"
+    }
+
+    @Throws(Exception::class)
+    private fun fetchRemoteFile(location: String): ByteArray? {
+        val url = URL(location)
+        var `is`: InputStream? = null
+        var bytes: ByteArray? = null
+        try {
+            `is` = url.openStream()
+            bytes = IOUtils.toByteArray(`is`)
+        } catch (e: IOException) {
+            //handle errors
+        } finally {
+            if (`is` != null) `is`!!.close()
+        }
+        return bytes
     }
 }
